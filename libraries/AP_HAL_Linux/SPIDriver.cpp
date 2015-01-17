8419b437c90a5410ad454a4b4af655128eb2a7d1
Mikhail Avkhimenia <mikhail.avkhimenia@emlid.com>
2015-01-17 20:02:20 +0300

    HAL_Linux: Add SPI configuration for NAVIO_ODROID_C1

--- libraries/AP_HAL_Linux/SPIDriver.cpp
+++ libraries/AP_HAL_Linux/SPIDriver.cpp
@@ -38,6 +38,12 @@ LinuxSPIDeviceDriver LinuxSPIDeviceManager::_device[LINUX_SPI_DEVICE_NUM_DEVICES
     LinuxSPIDeviceDriver(0, 0, AP_HAL::SPIDevice_MPU9250, SPI_MODE_0, 8, RPI_GPIO_7,  1*MHZ, 16*MHZ),
     LinuxSPIDeviceDriver(0, 0, AP_HAL::SPIDevice_Ublox, SPI_MODE_0, 8, RPI_GPIO_8,  1*MHZ, 4*MHZ),
 };
+#elif CONFIG_HAL_BOARD_SUBTYPE == HAL_BOARD_SUBTYPE_LINUX_NAVIO_ODROID_C1
+LinuxSPIDeviceDriver LinuxSPIDeviceManager::_device[LINUX_SPI_DEVICE_NUM_DEVICES] = {
+    /* MPU9250 is restricted to 1MHz for non-data and interrupt registers */
+    LinuxSPIDeviceDriver(0, 0, AP_HAL::SPIDevice_MPU9250, SPI_MODE_3, 8, ODROID_C1_GPIO_118,  1*MHZ, 20*MHZ),
+    //LinuxSPIDeviceDriver(0, 0, AP_HAL::SPIDevice_Ublox, SPI_MODE_0, 8, ODROID_C1_GPIO_117,  1*MHZ, 5*MHZ), //TODO
+};
 #else
 // empty device table
 LinuxSPIDeviceDriver LinuxSPIDeviceManager::_device[0];
